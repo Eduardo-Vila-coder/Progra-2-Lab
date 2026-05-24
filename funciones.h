b9c3ec4a3b810ff6ab55c72f8563c578e5e03ed5
Angel-842-dev <angelcotrinacruzado@gmail.com>
2026-05-23 19:49:08 -0500

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -9,16 +9,8 @@ void placeTower(int x, int y, int &money, int ** gameMap, int towersPosition[6][
 void spawnEnemies(int enemiesPosition[6][2], bool enemiesActivate[6]);
 void moveEnemies(int enemiesPosition[6][2], const bool enemiesActivate[6], int**& gameMap);
 void attackEnemies(int** gameMap, int &money, int enemiesPosition[6][2], bool enemiesActivate[6], int towersPosition[6][2], bool towersActivate[6]);
-void verifyResult();
-void gameStatus();
+void verifyResult(int* posicEnemys, bool* vivoEnemys, int numEnemys, int posicBase, bool attack_Iniciado, bool& finDelJuego);
+void gameStatus(int dinero, int torresColocadas, bool* vivoEnemys, int numEnemys);
 void deleteMap(int**& gameMap, const int& length_side);
 
 
-#ifndef FUNCIONES_H
-#define FUNCIONES_H
-
-int verifyResult(int* posicEnemys, bool* vivoEnemys, int numEnemys, int posicBase, bool attack_Iniciado);
-
-void gameStatus(int dinero, int torresColocadas, bool* vivoEnemys, int numEnemys);
-
-#endif
