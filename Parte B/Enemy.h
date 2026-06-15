ff1a4c7e9015392cc072163ab424a12bfbceedfe
JohanY2442 <johan.huayta@utec.edu.pe>
2026-06-15 08:11:48 -0500

    Update Enemy.h

--- Parte B/Enemy.h
+++ Parte B/Enemy.h
@@ -7,6 +7,26 @@
 
 
 class Enemy {
+private:
+  int salud;
+  bool activo;
+  int fila;
+  int columna;
+
+public:
+  Enemy();
+  Enemy(int salud, int fila, int columna);  // No escribi int activo porque todo enemigo nace vivo, osea activo == true
+
+  int getSalud() const;
+  bool getActivo() const;
+  int getFila() const;
+  int getColumna() const;
+
+  void setSalud(int salud);
+  void setActivo(bool activo);
+  void setPosicionEnemy(int fila, int columna);
+
+  bool operator==(const Enemy& otroEnemigo) const;
 };
 
 
