25e8f952c1e13bd86b7735639b5dd727ad8905af
Acer <indira.yabar@utec.edu.pe>
2026-06-15 14:02:35 -0500

    Class Tower

--- Parte B/Tower.cpp
+++ Parte B/Tower.cpp
@@ -3,3 +3,76 @@
 //
 
 #include "Tower.h"
+
+Tower::Tower() {
+    this->attackRange=2;
+    this->damage=2;
+    this->shots=5;
+    this->towerActivate=false;
+}
+
+Tower::Tower(int row, int col, int damage, int shots, int attackRange) {
+    this->damage=damage;
+    this->row=row;
+    this->col=col;
+    this->shots=shots;
+    this->attackRange=attackRange;
+    this->towerActivate = true;
+}
+
+Tower::~Tower() {
+
+};
+
+//getters and setters
+int Tower::getRow() {
+    return this->row;
+};
+int Tower::getCol() {
+    return this->col;
+};
+int Tower::getDamage() {
+    return this->damage;
+};
+bool Tower::getTowersActivate() {
+    return this->towerActivate;
+};
+int Tower::getShots() {
+    return this->shots;
+};
+int Tower::getAttackRange() {
+    return this->attackRange;
+};
+
+void Tower::setTowersActivate(bool towersActivate) {
+    this->towerActivate=towersActivate;
+};
+void Tower::setShots(int shots) {
+    this->shots=shots;
+};
+void Tower::setRow(int row) {
+    this->row=row;
+};
+void Tower::setCol(int col) {
+    this->col=col;
+};
+void Tower::setDamage(int damage) {
+    this->damage=damage;
+};
+void Tower::setAttackRange(int attackRange) {
+    this->attackRange=attackRange;
+};
+
+//funciones:
+//#1 restar la cantidad de disparos y desactivar la torre cuando ya no tenga disparos disponibles
+void Tower:: newShots() {
+   if (shots>0)
+       shots--;
+   if (shots==0)
+       towerActivate=false;
+};
+//#2 creamos una funcion que nos indica si la torre puede disparar o no (dependiendo de la cantidad de disparos que tenga)
+bool Tower:: canShot() {
+    if (towerActivate==true && shots>=0)
+        return true;
+};
--- Parte B/Tower.h
+++ Parte B/Tower.h
@@ -5,8 +5,46 @@
 #ifndef PARTE_B_TOWER_H
 #define PARTE_B_TOWER_H
 
+//Permita multiples disparos
+//Tenga rango de ataque
+//Tenga dano configurable (1 - 5)
+
 
 class Tower {
+private:
+    int row; //coordenadas de la torre
+    int col;
+    int attackRange; //rango de ataque
+    int damage; //daño causado
+    bool towerActivate; //para verificar si la torre está activa
+    int shots; //cantidad de disparos que tiene la torre, se disminuirá conforme los ejecute
+
+    public:
+    Tower(); //constructor por defecto
+    Tower(int row, int col, int damage, int shots, int attackRange); //constructor de asignación
+    ~Tower();
+
+    //getters and setters
+    int getRow();
+    int getCol();
+    int getDamage();
+    bool getTowersActivate();
+    int getShots();
+    int getAttackRange();
+
+    void setTowersActivate(bool towersActivate);
+    void setShots(int shots);
+    void setRow(int row);
+    void setCol(int col);
+    void setDamage(int damage);
+    void setAttackRange(int attackRange);
+
+    //funciones:
+    //#1 restar la cantidad de disparos y desactivar la torre cuando ya no tenga disparos disponibles
+    void newShots();
+    //#2 creamos una funcion que nos indica si la torre puede disparar o no (dependiendo de la cantidad de disparos que tenga)
+    bool canShot();
+
 };
 
 
