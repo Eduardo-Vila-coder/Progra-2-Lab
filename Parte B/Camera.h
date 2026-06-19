b8bd6b81a9bbefcd2d92d2a42cbfbd15481f196b
Angel-842-dev <angelcotrinacruzado@gmail.com>
2026-06-18 20:37:28 -0500

    Update Camera.h

--- Parte B/Camera.h
+++ Parte B/Camera.h
@@ -7,27 +7,29 @@
 
 class Camera {
 private:
-int x,y;
-int ancho, alto;
-int limiteAncho;
-int limiteAlto;
+    int x,y;
+    int ancho, alto;
+    int limiteAncho;
+    int limiteAlto;
 
 public:
-Camera(int ancho_Visor, int alto_Visor, int ancho_Map, int alto_Map);
-~Camera();
+    Camera(int ancho_Visor, int alto_Visor, int ancho_Map, int alto_Map);
+    ~Camera();
 
-void mover(int movX, int movY);
-void fijarPosicion(int nuevoX, int nuevoY);
+    void mover(int movX, int movY);
+    void fijarPosicion(int nuevoX, int nuevoY);
 
-int obtenX() const;
-int obtenY() const;
-int obtenAncho() const;
-int ibtenAlto() const;
+    int obtenX() const;
+    int obtenY() const;
+    int obtenAncho() const;
+    int obtenAlto() const;
 
-void mostrar(int** mapa, int posiTorres[6][2], int posiEnemies[6][2], bool torresActiv[6], bool enemiesActiv[6]) const;
+    void mostrar(int** mapa, int posiTorres[6][2], int posiEnemies[6][2], bool torresActiv[6], bool enemiesActiv[6]) const;
 
-void operator()(int nuevoX, int nuevoY);
-bool operator==(const Camera& otraCamera) const;
+    void operator()(int nuevoX, int nuevoY);
+    bool operator==(const Camera& otraCamera) const;
 };
+
+#endif //PARTE_B_CAMERA_H
 
 #endif //PARTE_B_CAMERA_H
