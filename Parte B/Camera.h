11e2fa86245b21558994f310396e275457ce3595
Angel-842-dev <angelcotrinacruzado@gmail.com>
2026-06-18 20:26:27 -0500

    Update Camera.h

--- Parte B/Camera.h
+++ Parte B/Camera.h
@@ -17,7 +17,7 @@ Camera(int ancho_Visor, int alto_Visor, int ancho_Map, int alto_Map);
 ~Camera();
 
 void mover(int movX, int movY);
-void fijarPosicion(int nuev_X, int nuev_Y);
+void fijarPosicion(int nuevoX, int nuevoY);
 
 int obtenX() const;
 int obtenY() const;
@@ -26,7 +26,7 @@ int ibtenAlto() const;
 
 void mostrar(int** mapa, int posiTorres[6][2], int posiEnemies[6][2], bool torresActiv[6], bool enemiesActiv[6]) const;
 
-void operator()(int nuev_X, int nuev_Y);
+void operator()(int nuevoX, int nuevoY);
 bool operator==(const Camera& otraCamera) const;
 };
 
