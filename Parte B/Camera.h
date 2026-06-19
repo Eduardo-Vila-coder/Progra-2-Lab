39c5d62e78957a3b78daa145223bff59348e14a5
Angel-842-dev <angelcotrinacruzado@gmail.com>
2026-06-18 19:59:49 -0500

    Update Camera.h

--- Parte B/Camera.h
+++ Parte B/Camera.h
@@ -5,9 +5,29 @@
 #ifndef PARTE_B_CAMERA_H
 #define PARTE_B_CAMERA_H
 
-
 class Camera {
-};
+private:
+int x,y;
+int ancho, alto;
+int limiteAncho;
+int limiteAlto;
 
+public:
+Camera(int ancho_Visor, int alto Visor, int ancho_Map, int alto_Map);
+~Camera();
+
+void mover(int movX, int movY);
+void fijarPosicion(int nuev_X, int nuev_Y);
+
+int obtenX() const;
+int obtenY() const;
+int obtenAncho() const;
+int ibtenAlto() const;
+
+void mostrar(int** mapa, int posiTorres[6][2], int posiEnemies[6][2], bool torresActiv[6], bool enemiesActiv[6]) const;
+
+void operator()(int nuev_X, int nuev_Y);
+bool operator==(const Camera& otraCamera) const;
+};
 
 #endif //PARTE_B_CAMERA_H
