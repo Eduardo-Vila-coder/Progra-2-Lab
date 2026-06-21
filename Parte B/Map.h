9fa168af8add2497282397165b63269eb28ca758
JohanY2442 <johan.huayta@utec.edu.pe>
2026-06-20 22:35:57 -0500

    Update Map.h

--- Parte B/Map.h
+++ Parte B/Map.h
@@ -5,39 +5,21 @@
 #ifndef PARTE_B_MAP_H
 #define PARTE_B_MAP_H
 
-#include "Enemy.h"
-#include "Tower.h"
-
 class Map {
 private:
-    int** mapa;
-    int tamanio_lado;
-
-    // Utilizaremos arreglo dinamico de punteros
-    // Map va a agregar enemigos y torres pero no necesariamente va a ser quien los destruye
-    Enemy** enemigos;
-    Tower** torres;
-
-    int cantidad_enemigos;
-    int cantidad_torres;
+    int** gameMap;
+    int length_side;
 
 public:
     Map();
-    Map(int tamanio_lado);
+    Map(int length_side);
     ~Map();
-
-    int** getMapa() const;
-    int getTamanioLado() const;
 
     void crearMapa();
     void crearCamino();
 
-    // Estos metodos se completaran despues usando Enemy y Tower
-    void placeTower(int fila, int columna);
-    void spawnEnemies();
-    void moveEnemies();
-    void attackEnemies();
-    void verifyResult(bool& victoria, bool& derrota);
+    int** getGameMap() const;
+    int getLengthSide() const;
 };
 
 #endif //PARTE_B_MAP_H
