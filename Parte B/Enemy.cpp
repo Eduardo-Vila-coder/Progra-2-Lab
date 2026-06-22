635d03037c26f774883c397ce8af8ca2c6d92f00
JohanY2442 <johan.huayta@utec.edu.pe>
2026-06-22 09:39:26 -0500

    Update Enemy.cpp

--- Parte B/Enemy.cpp
+++ Parte B/Enemy.cpp
@@ -51,21 +51,17 @@ void Enemy::setPosicionEnemy(int fila, int columna) {
 }
 
 // En esta parte, esta la sobrecarga del operador, que permite restar puntos de salud a un enemigo usando el operador -
-void Enemy::operator-=(int puntos) const {
-  Enemy enemigo_resultado = *this;
-
-  if (puntos >= enemigo_resultado.salud){  // Si el daño es mayor o igual a la salud que tiene el enemigo, la salud quedaria en cero
-    enemigo_resultado.salud = 0;
-    enemigo_resultado.activo = false;
+void Enemy::operator-=(int puntos) {
+  if (puntos >= salud) {
+    salud = 0;
+    activo = false;
   } else {
-    enemigo_resultado.salud = enemigo_resultado.salud - puntos;
+    salud = salud - puntos;
   }
-
-  return enemigo_resultado;
 
 }
 
 // Esta funcion recibe el danio de la torre y usa la sobrecarga del operador -
 void Enemy::enemigo_en_ataque(int danio_torre) {
-  *this = *this - danio_torre;
+  *this -= danio_torre;
 }
