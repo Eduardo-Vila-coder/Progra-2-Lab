ab2ccbc391add3fe2d9ea3934cb436bb949ccde9
JohanY2442 <johan.huayta@utec.edu.pe>
2026-06-22 09:47:38 -0500

    Update Map.h

--- Parte B/Map.h
+++ Parte B/Map.h
@@ -1,10 +1,18 @@
 #ifndef PARTE_B_MAP_H
 #define PARTE_B_MAP_H
+#include <vector>
+#include "Enemy.h"
+#include "Tower.h"
 
 class Map {
 private:
     int** gameMap;
     int length_side;
+
+    // Agregacion: Lo que va a hacer Map es guardar punteros a enemigos y torres
+    vector <Enemy*> enemigos;
+    vector <Tower*> torres;
+    
 
 public:
     Map();
