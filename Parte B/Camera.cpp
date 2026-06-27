93f7731d99b5f473de27351051875b0229b9cb67
JohanY2442 <johan.huayta@utec.edu.pe>
2026-06-26 20:13:09 -0500

    Update Camera.cpp

--- Parte B/Camera.cpp
+++ Parte B/Camera.cpp
@@ -17,18 +17,12 @@ Camera::Camera(int ancho_Visor, int alto_Visor, int anchoMap, int altoMap) {
 Camera::~Camera() {
 }
 
-void Camera::fijarPosicion(int nuevoX, int nuevoY) {
-    x = nuevoX;
-    y = nuevoY;
-
-    if (x < 0) x = 0;
-    if (y < 0) y = 0;
-    if (x > limiteAncho - ancho) x = limiteAncho - ancho;
-    if (y > limiteAlto - alto) y = limiteAlto - alto;
-}
 
 void Camera::mover(int movX, int movY) {
-    fijarPosicion(x + movX, y + movY);
+    int nuevoX = x + movX;
+    int nuevoY = y + movY;
+
+    operator()(nuevoX, nuevoY);
 }
 
 int Camera::obtenX() const { return x; }
@@ -83,6 +77,15 @@ void Camera::drawWindow(int** mapa, vector<Tower*>& torres, vector<Enemy*> enemi
     }
 }
 
-void Camera::operator()(int nuev_X, int nuev_Y) {
-    fijarPosicion(nuev_X, nuev_Y);
+void Camera::operator()(int nuevoX, int nuevoY) {
+    x = nuevoX;
+    y = nuevoY;
+
+    if (x < 0) x = 0;
+    
+    if (y < 0) y = 0;
+    
+    if (x > limiteAncho - ancho) x = limiteAncho - ancho;
+    
+    if (y > limiteAlto - alto) y = limiteAlto - alto;
 }
