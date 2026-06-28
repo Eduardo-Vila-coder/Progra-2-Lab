13551055cef6e14f8e54dd0f5ad3adce3ea1764e
sofxh <sofia.antunez@utec.edu.pe>
2026-06-28 18:13:35 -0500

    Update Map.h
    solo movi los comentarios para q se vea todo en la ppt

--- Parte B/Map.h
+++ Parte B/Map.h
@@ -12,7 +12,8 @@ private:
     int** gameMap;
     int length_side;
 
-    //Creamos un arreglo local con las posiciones iniciales de todos los enemigos considerando el numero maximo de ellos que puede aparecer
+    //Creamos un arreglo local con las posiciones iniciales de todos los enemigos
+    //considerando el numero maximo de ellos que puede aparecer
     int start_position[6][2]={{0, 1}, {0, 2}, {0, 3},{0, 4}, {1, 4}, {2, 4}};
 
     // Composicion: Lo que va a hacer Map es guardar punteros a enemigos y torres
